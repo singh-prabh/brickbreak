b04798eb9f93568305e955a3a90532f2228f9450
Andrea Tomarelli <andrea@MacBook-Pro-di-Andrea.local>
2015-02-21 15:25:39 +0100

    Initial commit from local git

---
+++ BrickBreak/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BrickBreak
+//
+//  Created by Andrea Tomarelli on 21/02/15.
+//  Copyright (c) 2015 coCoffee Team. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BrickBreak/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BrickBreak
+//
+//  Created by Andrea Tomarelli on 21/02/15.
+//  Copyright (c) 2015 coCoffee Team. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
