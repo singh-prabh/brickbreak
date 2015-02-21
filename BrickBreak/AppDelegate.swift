ec796f4aa023cad376f147e261c9ebc583132d3a
Andrea Tomarelli <andrea@MacBook-Pro-di-Andrea.local>
2015-02-21 15:45:58 +0100

    Commit semplice

--- BrickBreak/AppDelegate.swift
+++ BrickBreak/AppDelegate.swift
@@ -11,16 +11,13 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        println("Hello there")
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
 }
-
